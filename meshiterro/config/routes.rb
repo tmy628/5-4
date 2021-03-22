ea7042e1150e504cf7db80229e8cb70a2923175e
tmy628 <tmy6h1r@gmail.com>
2021-03-22 15:02:14 +0000

    [Finish] 5-4

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'post_images#index'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
